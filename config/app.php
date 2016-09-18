730178d37762f0d4dfa88aee89378127031f20db
Daniel France <dfrance1@Daniels-MacBook-Pro.local>
2016-09-18 14:59:53 -0500

    first dev commit

--- config/app.php
+++ config/app.php
@@ -39,7 +39,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+
+
+    'url' => env('APP_URL', 'http://dev.vpilv.com'),
 
     /*
     |--------------------------------------------------------------------------
