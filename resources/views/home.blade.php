2f467daeebf80c5e1603b1d7c8eda2c6cf1ab7f3
Daniel France <danielfrance@Daniels-MacBook-Pro.local>
2017-01-05 11:47:18 -0600

    updates per steve

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,26 +71,27 @@
             <header id="family" class="heading-a">
                 <h3><span class="small">Our Family</span> <span class="strong">Meet our</span> Family</h3>
             </header>
-            <div class="ui grid">
-                <div class="six wide column">
-                        <ul id='founder' class="gallery-b">
-                            <li><a href="#"><img src="assets/images/staff/jack.jpg" alt="Jack Chanin" > </a>
-                                <div>
-                                    <h4><span>Jack Chanin</span></h4>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="eight wide column">
-                        <h5><span class="small">Our Founder</span> <span class="strong">Jack Chanin</span> </h5>
-                        <p>Jack Chanin was born in Brooklyn, N.Y. on October 24, l930. His parents, George and Hilda were born in Riga, Latvia as were his three siblings. In 1922 George left Latvia for the United States to make a better life for himself and his family. He left his wife, daughters, Bella and Edith and son Meyer behind and promised to send for them as soon as he saved enough money for their passage by ship. Seven years later that day finally arrived. Hilda packed up her children, their few belongings and made the voyage to the United States. It was a difficult journey and the ship was crowded and dirty. Many of the passengers became ill and the stench of feces and vomit permeated the air. Hilda said when she saw the Statue of Liberty, first she cried and then she thanked God that they were finally in the “promised” land.
-Jack was born barely a year after Hilda’s arrival and now their family was complete. Since he was so much younger than his siblings he was always considered the “baby” of the family. Bella, who was 10 years older than Jack, took on the role of mothering him.
-Jack was always fascinated by photography and attended Metropolitan High School where photography and printing courses were offered. He graduated in June of 1948. In 1950 he enlisted in the United States Air Force where he was assigned to the 51st Fighter Interceptor Wing and was trained as a Gun Camera Technician. As the fighter planes returned from their mission Jack would unload and process the films which documented the war as it was unfolding. He was also assigned to document the day to day activities of his fellow airmen and to capture the war on “still photography”. In 1954 he was honorably discharged and returned home to Brooklyn.
-In 1955 he applied for and got a job at a small advertising and photography company which was then called Plymouth Photographers. He was hired as a messenger boy, but through hard work and his desire to learn all he could, he advanced up the ranks, and he became a valuable “inside man” who could perform in any and all capacities. Jack now held the title of Production Manager, and the company continued to grow. In 1966 Plymouth Photographers’ name was changed to Visual Promotions, Inc. and was now a major force in the printing and photography field, and one to be reckoned with. Soon thereafter they expanded their printing capacities and opened Kordet Color, Inc. a beautiful, modern printing plant. The facilities boasted the most modern, innovative printing presses and the company eventually had 3 shifts working 24 hours a day. In 1988 Jack decided to relocate to Las Vegas and said his fond farewell to VPI and his longtime co-workers and friends. In August of 1988 Jack bought a tiny print shop on Valley View Blvd. He just loved getting up each morning and going to work. He often said that professionally he went backwards 20 years, but he wouldn’t have traded his new found company for the world. Greta, his younger daughter started working with him and he trained her just as he had been trained. She started as a “runner” and learned to do developing, plating, etc. in her spare time when there was nothing to deliver. Jack made sure that she did not receive any special treatment and if she wanted to work at Valley Press she would move up through the ranks just as he had. Soon thereafter Jack realized that Greta was a great salesperson….she now knew each and every aspect of the business and could sell a job based on solid knowledge and could describe each and every step that was involved in producing a superior finished product for the customer’s specific job. It was then that this father-daughter team was actually born….and the rest is history!
-On a more personal level, we will begin in 1962. It was then that he met Elaine Althoff, a beautiful girl 13 years his junior. Friends and family were deeply concerned about the difference in age but both Jack and Elaine could not be swayed. They were deeply in love, and knew in their heart that nothing could prevent them from being together. They were married on June 1, l963. Their first child, Nancy was born in July, 1964, the second was a son, Jerry born in March, 1967 and finally their third child, Greta in April, l968. The Chanins were a close knit and loving family, every birthday, anniversary and every major holiday was celebrated with their large extended family in attendance.
-</p>
-                    </div>
 
+            <div class="row">
+                <div class="float left">
+                    <ul id='founder' class="gallery-b">
+                        <li><a href="#"><img src="assets/images/staff/jack.jpg" alt="Jack Chanin" > </a>
+                            <div>
+                                <h4><span>Jack Chanin</span></h4>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="right">
+                    <h5><span class="small">Our Founder</span> <span class="strong">Jack Chanin</span> </h5>
+                    <p>Jack Chanin was born in Brooklyn, N.Y. on October 24, l930. His parents, George and Hilda were born in Riga, Latvia as were his three siblings. In 1922 George left Latvia for the United States to make a better life for himself and his family. He left his wife, daughters, Bella and Edith and son Meyer behind and promised to send for them as soon as he saved enough money for their passage by ship. Seven years later that day finally arrived. Hilda packed up her children, their few belongings and made the voyage to the United States. It was a difficult journey and the ship was crowded and dirty. Many of the passengers became ill and the stench of feces and vomit permeated the air. Hilda said when she saw the Statue of Liberty, first she cried and then she thanked God that they were finally in the “promised” land.
+                        <br> <br>Jack was born barely a year after Hilda’s arrival and now their family was complete. Since he was so much younger than his siblings he was always considered the “baby” of the family. Bella, who was 10 years older than Jack, took on the role of mothering him.
+                        <br><br>Jack was always fascinated by photography and attended Metropolitan High School where photography and printing courses were offered. He graduated in June of 1948. In 1950 he enlisted in the United States Air Force where he was assigned to the 51st Fighter Interceptor Wing and was trained as a Gun Camera Technician. As the fighter planes returned from their mission Jack would unload and process the films which documented the war as it was unfolding. He was also assigned to document the day to day activities of his fellow airmen and to capture the war on “still photography”. In 1954 he was honorably discharged and returned home to Brooklyn.
+                        <br><br>In 1955 he applied for and got a job at a small advertising and photography company which was then called Plymouth Photographers. He was hired as a messenger boy, but through hard work and his desire to learn all he could, he advanced up the ranks, and he became a valuable “inside man” who could perform in any and all capacities. Jack now held the title of Production Manager, and the company continued to grow. In 1966 Plymouth Photographers’ name was changed to Visual Promotions, Inc. and was now a major force in the printing and photography field, and one to be reckoned with. Soon thereafter they expanded their printing capacities and opened Kordet Color, Inc. a beautiful, modern printing plant. The facilities boasted the most modern, innovative printing presses and the company eventually had 3 shifts working 24 hours a day.
+                        <br><br>In 1988 Jack decided to relocate to Las Vegas and said his fond farewell to VPI and his longtime co-workers and friends. In August of 1988 Jack bought a tiny print shop on Valley View Blvd. He just loved getting up each morning and going to work. He often said that professionally he went backwards 20 years, but he wouldn’t have traded his new found company for the world. Greta, his younger daughter started working with him and he trained her just as he had been trained. She started as a “runner” and learned to do developing, plating, etc. in her spare time when there was nothing to deliver. Jack made sure that she did not receive any special treatment and if she wanted to work at Valley Press she would move up through the ranks just as he had. Soon thereafter Jack realized that Greta was a great salesperson….she now knew each and every aspect of the business and could sell a job based on solid knowledge and could describe each and every step that was involved in producing a superior finished product for the customer’s specific job. It was then that this father-daughter team was actually born….and the rest is history!
+                        On a more personal level, we will begin in 1962. It was then that he met Elaine Althoff, a beautiful girl 13 years his junior. Friends and family were deeply concerned about the difference in age but both Jack and Elaine could not be swayed. They were deeply in love, and knew in their heart that nothing could prevent them from being together. They were married on June 1, l963. Their first child, Nancy was born in July, 1964, the second was a son, Jerry born in March, 1967 and finally their third child, Greta in April, l968. The Chanins were a close knit and loving family, every birthday, anniversary and every major holiday was celebrated with their large extended family in attendance.
+                    </p>
+                </div>
 
             </div>
             <ul class="gallery-b">
@@ -121,7 +122,7 @@ On a more personal level, we will begin in 1962. It was then that he met Elaine 
                 </li>
                 <li><img class='team' src="assets/images/staff/steve.jpg" alt="Steve Smith" >
                     <div>
-                        <h4><span>Steve Smith</span></h4>
+                        <h4><span>Steve Stone</span></h4>
 
                     </div>
                 </li>
